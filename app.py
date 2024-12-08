a1bd338cb793b236e8e036f8261725c083cf5f6a
pkingGMU <pking23@gmu.edu>
2024-12-08 15:36:17 -0500

    Comment changes

--- app.py
+++ app.py
@@ -14,16 +14,18 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
-# A simple in-memory store for messages (in a real-world app, you'd use a database)
+# CHANGE MESSAGE STORAGE TO A DATABASE
 messages = []
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+# Home page using index.html. This is where I'll type message that will get stored
 @app.route('/')
 def index():
     return render_template('index.html', messages=messages)
 
+# Messages from the web interface
 @app.route('/submit_message', methods=['POST'])
 def submit_message():
     message = request.form['message']
@@ -31,6 +33,7 @@ def submit_message():
         messages.append(message)
     return redirect(url_for('index'))
 
+# Images from the web interface
 @app.route('/upload_image', methods=['POST'])
 def upload_image():
     if 'image' not in request.files:
@@ -45,6 +48,7 @@ def upload_image():
         messages.append(f"New image uploaded: {filename}")
     return redirect(url_for('index'))
 
+# API for the pico to get the latest message
 @app.route('/get_latest_message', methods=['GET'])
 def get_latest_message():
     latest_message = messages[-1] if messages else "No messages yet"
